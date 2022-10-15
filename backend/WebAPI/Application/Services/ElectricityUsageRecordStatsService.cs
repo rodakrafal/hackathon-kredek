db7602aec294eaf8578b67106bea7e6116e9f99a
jakubtadeusz <39742282+jakubtadeusz@users.noreply.github.com>
2022-10-16 00:35:36 +0200

    Implement average usage calculation (#5)
    * Implement average usage calculation

--- backend/WebAPI/Application/Services/ElectricityUsageRecordStatsService.cs
+++ backend/WebAPI/Application/Services/ElectricityUsageRecordStatsService.cs
@@ -1,5 +1,8 @@
 ﻿using Application.Interfaces;
+using AutoMapper;
 using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,15 +11,34 @@ using System.Threading.Tasks;
 
 namespace Application.Services
 {
-    public class ElectricityUsageRecordStatsService : IElectricityUsageRecordStatsService
+    public class ElectricityUsageRecordStatsService : Service, IElectricityUsageRecordStatsService
     {
+        public ElectricityUsageRecordStatsService(DataContext context, IMapper mapper) : base(context, mapper)
+        {
+        }
+
         public ElectricityUsageRecordStats GetElectricityUsageRecordStats(ElectricityUsageRecord electricityUsageRecord)
         {
+            var average = Context.ElectricityUsageRecords
+                .Include(x => x.Point)
+                .Include(x => x.Point.Area)
+                .Include(x => x.ElectricalAppliances)
+                .Where(x => x.Point.AreaId == electricityUsageRecord.Point.AreaId)
+                .ToList()
+                .Where(x => GetElectricityUsageGrade(x) >= GetElectricityUsageGrade(electricityUsageRecord) * 0.9)
+                .Where(x => GetElectricityUsageGrade(x) <= GetElectricityUsageGrade(electricityUsageRecord) * 1.1)
+                .Average(x => x.YearlyUsage);
+
             return new ElectricityUsageRecordStats()
             {
                 YearlyUsage = electricityUsageRecord.YearlyUsage,
-                AverageYearlyUsage = electricityUsageRecord.YearlyUsage
+                AverageYearlyUsage = (int)average
             };
+        }
+
+        private int GetElectricityUsageGrade(ElectricityUsageRecord record)
+        {
+            return record.ElectricalAppliances.Sum(x => x.Amount);
         }
     }
 }
